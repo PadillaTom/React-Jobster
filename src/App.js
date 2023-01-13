31a1eecfba9ecf98c8153b7d04b92f04618a710b
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-13 12:34:35 +0100

    main - bugfix routes deploy

--- src/App.js
+++ src/App.js
@@ -6,19 +6,9 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route
-					exact
-					path="/React-Jobster"
-					element={<Dashboard></Dashboard>}
-				></Route>
-				<Route
-					path="/React-Jobster/landing"
-					element={<Landing></Landing>}
-				></Route>
-				<Route
-					path="/React-Jobster/register"
-					element={<Register></Register>}
-				></Route>
+				<Route exact path="/" element={<Dashboard></Dashboard>}></Route>
+				<Route path="landing" element={<Landing></Landing>}></Route>
+				<Route path="register" element={<Register></Register>}></Route>
 				<Route path="*" element={<Error></Error>}></Route>
 			</Routes>
 		</BrowserRouter>
