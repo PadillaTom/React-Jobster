12c9599a41d7dcc93d055a48d49ac7b3a90e11d4
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-13 14:12:21 +0100

    main - routes OK, NO DASHBOARD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Dashboard, Landing, Register, Error } from "./pages";
 
 const App = () => {
 	return (
-		<BrowserRouter basename="/React-Jobster" element={<Dashboard></Dashboard>}>
+		<BrowserRouter basename="/React-Jobster">
 			<Routes>
+				<Route path="/" element={<Dashboard></Dashboard>}></Route>
 				<Route path="landing" element={<Landing></Landing>}></Route>
 				<Route path="register" element={<Register></Register>}></Route>
 				<Route path="*" element={<Error></Error>}></Route>
