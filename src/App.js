79340dc9ddcc2e15b028d39a758a0b677f0aefe4
Tomas Padilla <padillatomasagustin@gmail.com>
2023-01-13 12:52:14 +0100

    main - bugfix GithubPages with ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard, Landing, Register, Error } from "./pages";
 
 const App = () => {
 	return (
-		<HashRouter>
+		<BrowserRouter basename="/React-Jobster">
 			<Routes>
-				<Route
-					exact
-					path="/React-Jobster"
-					element={<Dashboard></Dashboard>}
-				></Route>
+				<Route exact path="/" element={<Dashboard></Dashboard>}></Route>
 				<Route path="landing" element={<Landing></Landing>}></Route>
 				<Route path="register" element={<Register></Register>}></Route>
 				<Route path="*" element={<Error></Error>}></Route>
 			</Routes>
-		</HashRouter>
+		</BrowserRouter>
 	);
 };
 
